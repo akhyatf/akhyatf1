c59cbd49b0cabebc2bb3eb3801e1f38dea5c0b15
akhyatf <32256402+akhyatf@users.noreply.github.com>
2017-11-07 19:59:32 +0800

    menampilkan hasil output dari Sebuah Pilihan

--- Praktek0302.cpp
+++ Praktek0302.cpp
@@ -4,4 +4,9 @@ using namespace std;
 
 int main(int argc, char** argv){
 	int pjg,lbr,l,pil,a,t;
+	
+	cout <<"1. Persegi Panjang\n";
+	cout <<"2. Segitiga\n";
+	cout <<"Masukkan Pilihan : ";
+	cin >>pil;
 }
