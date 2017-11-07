9bc10bea274fd6bf7f03824144f8f0516f24dc4f
akhyatf <32256402+akhyatf@users.noreply.github.com>
2017-11-08 00:02:07 +0800

    Menghitung dan menampilkan Hasil Perhitungan menggunakan variable IF-ELSE IF

--- Praktek0305.cpp
+++ Praktek0305.cpp
@@ -11,6 +11,17 @@ int main(){
 	cout <<"Masukkan Tinggi Badan Anda : ";
 	cin >>tinggi;
 	
-	
+	cout <<"Hasil Perhitungan Anda"<<endl;
+	cout<<"Berat Badan Anda : "<<berat<<endl;
+	cout<<"Tinggi Badan Anda : "<<tinggi<<endl;
+	if(berat < tinggi/2.5){
+		cout <<"Anda Underweight";
+	}
+	else if(tinggi/2.3 < berat){
+		cout <<"Anda Overweight";
+	}
+	else if(tinggi/2.5 <= berat <= tinggi/2.3){
+		cout <<"Anda Normal";
+	}
 	
 }
