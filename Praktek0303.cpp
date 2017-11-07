dac1e322234ecf40269f887656f3a442c4c79998
akhyatf <32256402+akhyatf@users.noreply.github.com>
2017-11-07 20:58:13 +0800

    Menampilkan dan menghitung Gaji Karyawan
    40 itu jam kerja pokoknya, kalau lebih 40 jam dihitung lembur, jam lembur dikali 1,5 dan dikali pajak penghasilan 0.15

--- Praktek0303.cpp
+++ Praktek0303.cpp
@@ -10,4 +10,18 @@ int main (){
 	cin >>gp;
 	cout <<"Masukkan Jam Kerja Anda : ";
 	cin>>jk;
+	
+	lmbr=jk-40;
+	gs=(lmbr*1.5+40)*gp;
+	pjk=gs*0.15;
+	gk=gs-pjk;
+	
+	cout <<endl;
+	cout <<"Gaji Pokok	: Rp. "<<gp<<endl;
+	cout <<"Jam Kerja	: "<<jk<<endl;
+	cout <<"Lembur 		: "<<lmbr<<endl;
+	cout <<"Gaji Sebelum Dipotong Pajak : Rp. "<<gs<<endl;
+	cout <<"Total Gaji Anda Adalah : "<<gk;
+	
+	
 }
