3e92efc26709bc191089c6e346def376ad14880a
akhyatf <32256402+akhyatf@users.noreply.github.com>
2017-11-07 20:25:57 +0800

    Menampilkan Hasil dari Penghitungan gaji karyawan dengan memasukkan gaji pokok dan jam kerja

--- Praktek0303.cpp
+++ Praktek0303.cpp
@@ -4,4 +4,10 @@ using namespace std;
 int main (){
 	int gp,jk;
 	int lmbr,gs,pjk,gk;
+	
+	cout <<"Penghitungan Gaji Karyawan"<<endl;
+	cout <<"Masukkan Gaji Pokok Anda : Rp. ";
+	cin >>gp;
+	cout <<"Masukkan Jam Kerja Anda : ";
+	cin>>jk;
 }
