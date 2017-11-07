d63c73c8b92198b0668ae0136b43b41cfdbe40cd
akhyatf <32256402+akhyatf@users.noreply.github.com>
2017-11-07 23:56:08 +0800

    Menampilkan output Hasil dari memasukkan Berat badan dan tinggi badan

--- Praktek0305.cpp
+++ Praktek0305.cpp
@@ -4,4 +4,13 @@ using namespace std;
 
 int main(){
 	double berat, tinggi;
+	
+	cout <<"Perhitungan Berat Badan Ideal"<<endl;
+	cout <<"Masukkan Berat Badan Anda : ";
+	cin >>berat;
+	cout <<"Masukkan Tinggi Badan Anda : ";
+	cin >>tinggi;
+	
+	
+	
 }
